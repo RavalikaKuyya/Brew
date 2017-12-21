92d234027726ebd825f67f088442c2aacb2b29bc
Jamie Sanson <jamie.h.sanson@gmail.com>
2017-12-21 17:39:17 +1300

    Adds dependencies and renames colours

--- app/src/main/java/io/github/jamiesanson/brew/BrewActivity.kt
+++ app/src/main/java/io/github/jamiesanson/brew/BrewActivity.kt
@@ -3,7 +3,10 @@ package io.github.jamiesanson.brew
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Launcher activity which acts as a splash screen, as well as intent filter.
+ */
+class BrewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
