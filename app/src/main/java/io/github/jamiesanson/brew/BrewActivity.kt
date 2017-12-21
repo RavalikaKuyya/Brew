f62cdf5f9d51eec063334c09cd1c3fb653b0f1cd
Jamie Sanson <jamie.h.sanson@gmail.com>
2017-12-21 20:08:34 +1300

    Adds splash screen

--- app/src/main/java/io/github/jamiesanson/brew/BrewActivity.kt
+++ app/src/main/java/io/github/jamiesanson/brew/BrewActivity.kt
@@ -2,6 +2,8 @@ package io.github.jamiesanson.brew
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.github.jamiesanson.brew.ui.main.MainActivity
+import org.jetbrains.anko.startActivity
 
 /**
  * Launcher activity which acts as a splash screen, as well as intent filter.
@@ -10,6 +12,9 @@ class BrewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // TODO - Intercept here to do intent filtering
+
+        startActivity<MainActivity>()
+        finish()
     }
 }
