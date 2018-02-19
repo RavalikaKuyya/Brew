fa1935169a23adeaeb18b91c4684001ae4ae7c66
Jamie Sanson <jamie.h.sanson@gmail.com>
2018-02-19 21:11:09 +1300

    Re-adds scrolling bottombar behavior

--- app/src/main/java/io/github/koss/brew/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/koss/brew/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
@@ -15,6 +16,7 @@ import android.view.ViewGroup
 import com.airbnb.epoxy.*
 import io.github.koss.brew.*
 import io.github.koss.brew.ui.main.MainActivity
+import io.github.koss.brew.ui.main.fragment.NestedScrollListener
 import io.github.koss.brew.util.RalewayRegular
 import io.github.koss.brew.util.anim.GravitySnapHelper
 import io.github.koss.brew.util.anim.RevealAnimationSettings
@@ -31,8 +33,10 @@ import kotlin.math.abs
 
 class HomeFragment : Fragment() {
 
-    @Inject lateinit var eventBus: UiEventBus
-    @Inject lateinit var viewModelFactory: BrewViewModelFactory
+    @Inject
+    lateinit var eventBus: UiEventBus
+    @Inject
+    lateinit var viewModelFactory: BrewViewModelFactory
     private lateinit var viewModel: HomeViewModel
     private var recyclerViewSavedState: Bundle? = null
 
@@ -43,7 +47,7 @@ class HomeFragment : Fragment() {
                 .of(activity as MainActivity, viewModelFactory)
                 .get(HomeViewModel::class.java)
 
-        Carousel.setDefaultGlobalSnapHelperFactory(object: Carousel.SnapHelperFactory() {
+        Carousel.setDefaultGlobalSnapHelperFactory(object : Carousel.SnapHelperFactory() {
             override fun buildSnapHelper(context: Context?): SnapHelper = GravitySnapHelper(Gravity.START)
         })
     }
@@ -52,6 +56,7 @@ class HomeFragment : Fragment() {
             inflater.inflate(R.layout.fragment_home, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        interceptScrollAndDispatchToParent()
         initialiseToolbar()
         applyTypeface()
         floatingActionButton.onClick { onAddClicked(true) }
@@ -109,7 +114,7 @@ class HomeFragment : Fragment() {
 
     private fun applyTypeface() {
         val typeface = Typeface.createFromAsset(context?.assets, RalewayRegular.path)
-        with (collapsingLayout) {
+        with(collapsingLayout) {
             setCollapsedTitleTypeface(typeface)
             setExpandedTitleTypeface(typeface)
         }
@@ -144,5 +149,25 @@ class HomeFragment : Fragment() {
                         .start()
             }
         }
+    }
+
+    /**
+     * This method is a bit of a hack. For the BottomNavigationView in the MainFragment to hide on
+     * scroll it needs to have access to callbacks fired by scrolling view layout behaviors. This
+     * doesn't work in the current setup, as it isn't a direct child of the CoordinatorLayout.
+     *
+     * Note: NestedScroll events can be dispatched from *any* nested child or parent, however a bug with
+     * CollapsingToolbarLayout causes events to not be dispatched to nested or parent behaviors.
+     */
+    private fun interceptScrollAndDispatchToParent() {
+        val params = scrollInterceptorView.layoutParams as CoordinatorLayout.LayoutParams
+        val behavior = ScrollInterceptionBehavior()
+        behavior.scrollCallback {
+            if (parentFragment is NestedScrollListener) {
+                (parentFragment as NestedScrollListener).onScroll(it)
+            }
+        }
+        params.behavior = behavior
+        scrollInterceptorView.requestLayout()
     }
 }
