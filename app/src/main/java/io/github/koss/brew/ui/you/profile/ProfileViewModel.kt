0ca126a111348c9f3f8aa4f6453c096ccedabe0d
Jamie Sanson <jamie.sanson@outlook.com>
2018-06-04 21:30:10 +1200

    Fixes activity empty state

--- app/src/main/java/io/github/koss/brew/ui/you/profile/ProfileViewModel.kt
+++ app/src/main/java/io/github/koss/brew/ui/you/profile/ProfileViewModel.kt
@@ -7,9 +7,11 @@ import android.arch.paging.PagedList
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import io.github.koss.brew.data.local.dao.DrinkDao
 import io.github.koss.brew.data.model.ActivitySource
 import io.github.koss.brew.data.model.FirestoreOnly
 import io.github.koss.brew.data.model.LocalOnly
+import io.github.koss.brew.data.model.NoActivity
 import io.github.koss.brew.data.remote.error.NotLoggedInException
 import io.github.koss.brew.data.remote.worker.util.await
 import io.github.koss.brew.repository.activity.ActivityRepository
@@ -19,7 +21,8 @@ import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
 class ProfileViewModel @Inject constructor(
-    private val activityRepository: ActivityRepository
+    private val activityRepository: ActivityRepository,
+    private val drinkDao: DrinkDao
 ): ViewModel() {
 
     val activityType: LiveData<ActivitySource> = MutableLiveData()
@@ -27,10 +30,15 @@ class ProfileViewModel @Inject constructor(
     fun loadActivity() {
         launch {
             val configWrapper = ConfigurationWrapper.blockingFetch()
+
             if (!configWrapper.shouldSyncDrinks) {
-                (activityType as MutableLiveData).postValue(
-                        LocalOnly(source = activityRepository.getLocalActivity())
-                )
+                if (drinkDao.getAllDrinks().isEmpty()) {
+                    (activityType as MutableLiveData).postValue(NoActivity())
+                } else {
+                    (activityType as MutableLiveData).postValue(
+                            LocalOnly(source = activityRepository.getLocalActivity())
+                    )
+                }
             } else {
                 val user = FirebaseAuth.getInstance().currentUser ?: throw NotLoggedInException()
 
@@ -44,12 +52,16 @@ class ProfileViewModel @Inject constructor(
                         .setPageSize(20)
                         .build()
 
-                (activityType as MutableLiveData).postValue(
-                        FirestoreOnly(
-                                query = query,
-                                config = config
-                        )
-                )
+                if (query.limit(1).get().await().isEmpty) {
+                    (activityType as MutableLiveData).postValue(NoActivity())
+                } else {
+                    (activityType as MutableLiveData).postValue(
+                            FirestoreOnly(
+                                    query = query,
+                                    config = config
+                            )
+                    )
+                }
             }
         }
     }
